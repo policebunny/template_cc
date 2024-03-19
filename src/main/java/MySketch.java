0fa6dd2d7d96519ce629a44bd8df158fda9e099a
bm-213790 <bm-213790@hs-weingarten.de>
2024-03-19 10:24:16 +0100

    image pixelation

--- src/main/java/MySketch.java
+++ src/main/java/MySketch.java
@@ -1,16 +1,19 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.opengl.PShader;
 import controlP5.*;
 
 
 public class MySketch extends PApplet {
+	public PImage img = loadImage("https://i.redd.it/tbw9xtpzbcf61.jpg");
+	float circler = 10;
 	ControlP5 cp5;
 	public void setup() {
 		this.getSurface().setResizable(true);
 		cp5 = new ControlP5(this);
-		cp5.addSlider("maxframe")
+		cp5.addSlider("circler")
 				.setPosition(10, 10)
-				.setRange(1, 10)
+				.setRange(1, 100)
 				.setValue(5)
 				.setSize(100, 20);
 		cp5.addSlider("decay")
@@ -18,6 +21,16 @@ public class MySketch extends PApplet {
 				.setRange(0.001f, 0.005f)
 				.setValue(5)
 				.setSize(100, 20);
+
+
+		img.resize(width, height);   //public ez
+		ellipseMode(CORNER);
+		noStroke();   //kreis soll keinen rand haben
+		background(0);
+
+
+		// saveFrame("pixeltest.jpg");
+
 	}
 
 	public void settings() {
@@ -31,5 +44,20 @@ public class MySketch extends PApplet {
 	public void draw(){
 		background(64);
 		ellipse(mouseX, mouseY, 20, 20);
+
+		float circleSize = circler;
+		for (int x = 0; x < width; x += circleSize) {
+			for (int y = -47; y < height; y += circleSize) {
+				fill(img.get(x, max(y,0)));
+				ellipse(x, y, circleSize, circleSize); //circle(x,y,circleSize) ist vom import
+				//benutze ellipse stattdessen, aber input 4 variablen ez circlesizecirclesize
+			}
+			circleSize *= 0.964;
+			circleSize = max(circleSize, 1);
+		}
+	}
+
+	public void frameUpdate() {
+
 	}
 }
