91b03b9fc116dc718b1f25dbb0b5b95c5cb3e5db
policebunny <136323006+policebunny@users.noreply.github.com>
2024-03-29 09:51:51 +0100

    invader funktionen erstellt

--- src/main/java/MySketch.java
+++ src/main/java/MySketch.java
@@ -9,6 +9,8 @@ public class MySketch extends PApplet {
 	int pixelsize = 7;
 	ControlP5 cp5;
 	PImage invader;
+	int center = pixelsize / 2;
+	int invadersize = width / pixelsize;
 	public void setup() {
 		this.getSurface().setResizable(true);
 		cp5 = new ControlP5(this);
@@ -27,33 +29,13 @@ public class MySketch extends PApplet {
 		background(0);
 		noStroke();   //kreis soll keinen rand haben
 		frameRate(1);
-		for (int x = 0; x <= 2; x++) {
-			colors[x] = getrandomcolor(); // ersten 3 sollen random farbe sein
-		}
-		for (int x = 3; x <= 5; x++) {
-			colors[x] = color(0); // schwarze farbe
-		}
+
+		FillColorArray(colors); // for schleifen in methode gepackt
+
 		// now columm and rows
-		int center = pixelsize / 2; //
-		int invadersize = width / pixelsize;
-		for (int row = 0; row < pixelsize; row++) {
-			for (int columm = 0; columm < pixelsize; columm++) {
-				fill(chooserandomcolor(colors));
-				// columm
 
-				rect(invadersize * columm, invadersize * row, invadersize, invadersize);
-				// invaderPixels - 1 - column, row
-				rect(invadersize * (pixelsize - 1 - columm), invadersize * row, invadersize, invadersize);
+		makeInvader(colors); // for schleifen in methode gepackt
 
-			}
-			// row..... symmetricColumnCount, row
-			fill(chooserandomcolor(colors));
-			if (0 != pixelsize % 2) {
-				rect(invadersize * center, invadersize * row, invadersize, invadersize);
-			}
-
-
-		}
 		invader = get();
 		background(0);
 		image(invader, 0, 0);
@@ -88,4 +70,37 @@ public class MySketch extends PApplet {
 		return colors_[floor(random(colors_.length))];
 		// floor() macht ein float zu final int
 	}
+
+	public int[] FillColorArray(int[] colors_) {
+		for (int x = 0; x <= 2; x++) {
+			colors[x] = getrandomcolor(); // ersten 3 sollen random farbe sein
+		}
+		for (int x = 3; x <= 5; x++) {
+			colors[x] = color(0); // schwarze farbe
+		}
+		return colors_;
+	}
+
+	public void makeInvader(int[] colors) {
+		// brauche array hier zum ausfüllen
+		for (int row = 0; row < pixelsize; row++) {
+			for (int columm = 0; columm < pixelsize; columm++) {
+				fill(chooserandomcolor(colors));
+				// columm
+
+				rect(invadersize * columm, invadersize * row, invadersize, invadersize);
+				// invaderPixels - 1 - column, row
+				rect(invadersize * (pixelsize - 1 - columm), invadersize * row, invadersize, invadersize);
+
+			}
+			// row..... symmetricColumnCount, row
+			fill(chooserandomcolor(colors));
+			if (0 != pixelsize % 2) {
+				rect(invadersize * center, invadersize * row, invadersize, invadersize);
+			}
+
+		}
+
+	}
+
 }
